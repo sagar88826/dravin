dc9c235e492c0f9a713961a36294b6ee09817e74
[sagar] <[sagarmandal88826@gmail.com]>
2022-11-21 21:41:11 +0530

    Developed Navigation Bar and Partially implemented Dark Mode

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar.js';
 import SideMenu from './Components/SideMenu/SideMenu.js';
+import { useState } from 'react';
 
 function App() {
+  const [mode, setMode] = useState('light')
+    const changeMode =()=>{
+        if(mode==="light")
+        {
+            setMode('dark')
+        }
+        else{
+            setMode('light')
+        }
+    }
   return (
 
   <>
-  <Navbar/>
   <SideMenu/>
+  <Navbar changeMode={changeMode} mode={mode}/>
+  
   
   </>
 
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,13 +1,27 @@
 import React from 'react'
 
 import './Navbar.css'
-function Navbar() {
+function Navbar(props) {
+   var con=true;
+const checkCondition=()=>{
+
+    if(props.mode==='dark'){
+        con=true;
+    }
+    else{
+        con=false;
+    }
+}
     return (
         <>
-        <div className='nav-main'>
-            {/* <div className='logo-nav'><img src={process.env.PUBLIC_URL+'/logo.png'} width="vw" alt="accessibility text"/></div>
-           <h1>Dravin</h1> */}
-        </div>
+            <div className={`nav-main set-${props.mode}`}>
+                <h1 className='nav-heading'>Dravin</h1>
+                <div className="toggle-mode" onClick={props.changeMode}>
+                    {con?<i class="bi bi-sun-fill"></i>:<i class="bi bi-moon-fill"></i>}
+                </div>
+                
+                
+            </div>
         </>
     )
 }
