9f35b737d35378a1294e91a8a1f34a80076598c5
Vishal Kumar Singh <kvishal07860@gmail.com>
2022-11-24 18:20:41 +0530

    not a bright day - added content box header

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import Navbar from './Components/Navbar/Navbar.js';
 import SideMenu from './Components/SideMenu/SideMenu.js';
 import { useDarkMode} from './Components/Navbar/useDarkMode'
 import MainFeed from './Components/MainFeed/MainFeed';
-import UploadBar from './Components/UserUpload/UploadBar';
+
 function App() {
 
   const [theme, toggleButton] = useDarkMode();
   if(theme==='dark'){
     document.body.style.backgroundColor='#28282B';
    }else{
-    document.body.style.backgroundColor='#fff';
+    document.body.style.backgroundColor='#F2F2EE';
    }
   
   return (
--- src/Components/MainFeed/MainFeed.js
+++ src/Components/MainFeed/MainFeed.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './MainFeed.css'
 import UploadBar from '../UserUpload/UploadBar'
+import avatar from '../Img/sideBar/avatar.jpg'
 
 function MainFeed() {
     return (
@@ -8,23 +9,24 @@ function MainFeed() {
             <div className="tile-container">
                 <div className='tile-1'>
                     <UploadBar></UploadBar>
-                    <div className="box">inside the tiles</div>
-                    <div className="box">inside the tiles</div>
-                    <div className="box">inside the tiles</div>
-                    <div className="box">inside the tiles</div>
-                    <div className="box">inside the tiles</div>
-                    <div className="box">inside the tiles</div>
-                    <div className="box">inside the tiles</div>
-                    <div className="box">inside the tiles</div>
-                    <div className="box">inside the tiles</div>
-                    <div className="box">inside the tiles</div>
+                    <div className="box">
+                        <div className="box-header">
+                            <figure><img src={avatar} alt="avatar" /></figure>
+                            <p>Dillon Nair</p>
+                            <i class="bi bi-three-dots"></i>
+                        </div>
+                        <div className="box-content">
+                                <img src={avatar} alt="avatar" />
+                        </div>
+                        <div className="box-footer"></div>
+                    </div>
                 </div>
                 <div className="tile-2">
-                   <div className="tile-2-child">
-                   <div className="side-box"></div>
-                   <div className="side-box"></div>
-                   <div className="side-box"></div>
-                   </div>
+                    <div className="tile-2-child">
+                        <div className="side-box"></div>
+                        <div className="side-box"></div>
+                        <div className="side-box"></div>
+                    </div>
                 </div>
             </div>
 
